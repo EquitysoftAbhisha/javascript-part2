094c9c9ff7c0fb08bbafc97a43fe3d8be2457b8c
EquitysoftAbhisha <119053491+EquitysoftAbhisha@users.noreply.github.com>
2022-12-07 17:48:10 +0530

    java-script how its work

---
+++ basic.js
@@ -0,0 +1,16 @@
+// how its works(javascript)
+
+
+// complilation
+// code execution 
+
+
+// why compilation 
+
+// how javascript code executes
+
+// what is global exection context ?
+// what is local execution context /
+// clouseres
+
+
